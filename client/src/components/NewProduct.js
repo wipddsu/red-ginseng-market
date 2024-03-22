c7d952b6a4ab99a102b6de06d55486cbb074a921
wipddsu <rlaalstn8158@naver.com>
2024-03-22 11:41:58 +0900

    상품등록 폼 유효성 검사 추가

--- client/src/components/NewProduct.js
+++ client/src/components/NewProduct.js
@@ -2,27 +2,27 @@ import { useState } from 'react';
 import style from '../style/NewProduct.module.css';
 
 export default function NewProduct() {
-  const [isErrors, setIsErrors] = useState({});
+  const [isErrors, setIsErrors] = useState([]);
 
   function handleSubmit(e) {
     e.preventDefault();
 
     const fd = new FormData(e.target);
     const data = Object.fromEntries(fd.entries());
-    const emptyData = {};
 
     for (const key in data) {
       if (data[key] === '') {
-        emptyData[key] = true;
+        setIsErrors((prev) => [...prev, key]);
       } else {
-        emptyData[key] = false;
+        setIsErrors((prev) => prev.filter((item) => item !== key));
       }
     }
 
-    setIsErrors((prev) => ({
-      ...prev,
-      ...emptyData,
-    }));
+    for (const key in data) {
+      if (data[key] === '') {
+        return;
+      }
+    }
 
     const acquisitionCategory = fd.getAll('category');
     data.category = acquisitionCategory;
@@ -36,12 +36,12 @@ export default function NewProduct() {
           <div>
             <label htmlFor="title">상품명</label>
             <input type="text" id="title" name="title" className={style.input} />
-            {isErrors.title && <p>상품명을 입력해주세요.</p>}
+            {isErrors.includes('title') && <p>상품명을 입력해주세요.</p>}
           </div>
           <div>
             <label htmlFor="image">이미지 링크</label>
             <input type="text" id="image" name="image" />
-            {isErrors.image && <p>이미지 링크를 입력해주세요.</p>}
+            {isErrors.includes('image') && <p>이미지 링크를 입력해주세요.</p>}
           </div>
           <div>
             <label htmlFor="category">카테고리</label>
@@ -59,12 +59,12 @@ export default function NewProduct() {
           <div>
             <label htmlFor="price">가격</label>
             <input type="number" id="price" name="price" />
-            {isErrors.price && <p>가격을 입력해주세요.</p>}
+            {isErrors.includes('price') && <p>가격을 입력해주세요.</p>}
           </div>
           <div>
             <label htmlFor="content">내용</label>
             <textarea type="number" id="content" name="content" rows={10} />
-            {isErrors.price && <p>내용을 입력해주세요.</p>}
+            {isErrors.includes('content') && <p>내용을 입력해주세요.</p>}
           </div>
           <button>등록</button>
         </form>
